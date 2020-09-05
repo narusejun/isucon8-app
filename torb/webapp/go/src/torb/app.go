59a8ef2f7e66f78962155422b6064652173c78fb
to-hutohu <to-hutohu@to-hutohunoMacBook-Pro.local>
2020-09-05 14:52:18 +0900

    echo v3

--- torb/webapp/go/src/torb/app.go
+++ torb/webapp/go/src/torb/app.go
@@ -20,9 +20,9 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/sessions"
+	"github.com/labstack/echo"
 	"github.com/labstack/echo-contrib/session"
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"github.com/labstack/echo/middleware"
 )
 
 type User struct {
